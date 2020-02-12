c28ff9c288507ce929a8cf12d8cf14deec27b726
Pramod Kumar <pramod.kumar2@publicissapient.com>
2020-02-12 17:19:22 +0530

    message changed

--- StringExtension.swift
+++ StringExtension.swift
@@ -14,7 +14,7 @@ extension UnicodeScalar {
         
         switch value {
         case 0x3030, 0x00AE, 0x00A9, // Special Characters
-        0x1D000 ... 0x1F77F, // Emoticons
+        0x1D000 ... 0x1F77F, // Emoticons range
         0x2100 ... 0x27BF, // Misc symbols and Dingbats
         0xFE00 ... 0xFE0F, // Variation Selectors
         0x1F900 ... 0x1F9FF: // Supplemental Symbols and Pictographs
